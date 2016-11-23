ea8ea387ea88d7052be45e7651f73a95402d8775
Filip Bober <f.bober@algotronic.pl>
2016-11-23 12:06:38 +0100

    Add optional title property

--- RSB.Modules.Templater.Common/Contracts/TemplateAttribute.cs
+++ RSB.Modules.Templater.Common/Contracts/TemplateAttribute.cs
@@ -5,5 +5,11 @@ namespace RSB.Modules.Templater.Common.Contracts
     [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
     public sealed class TemplateAttribute : Attribute
     {
+        public string Title { get; }
+
+        public TemplateAttribute(string title = "")
+        {
+            Title = title;
+       }
     }
 }
